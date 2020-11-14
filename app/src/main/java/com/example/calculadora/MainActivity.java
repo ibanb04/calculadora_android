a20542b7ffb47b65440a2ac8a78af4274a77cf34
“ivan” <ibanb040@gmail.com>
2020-11-14 10:32:04 -0500

    interfaz grafica creada

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText num1, num2;
+    private TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1 = findViewById(R.id.txtNumeroUno);
+        num1 = findViewById(R.id.txtNumeroDos);
+        resultado = findViewById(R.id.lblResultado);
+    }
+
+    public void calcular(View v){
+        double n1,n2, suma;
+        suma =  m1 +´n2 
     }
 }
